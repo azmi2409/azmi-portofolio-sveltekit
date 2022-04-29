9c55c1e3f09161fe47ca7b485a3b82682f3a2667
azmi2409 <azmimuwahid@gmail.com>
2022-04-29 23:36:17 +0700

    fix sidebar and cursor

--- src/components/Pointer.svelte
+++ src/components/Pointer.svelte
@@ -11,5 +11,6 @@
 />
 
 <svg class="absolute top-0 left-0 w-full h-full m-['-8px'] z-50 pointer-events-none">
-	<circle class="fill-accent" cx={x} cy={y} r={$size} />
+	<circle class="stroke-accent fill-transparent" stroke-width="2" cx={x} cy={y} r={$size} />
+	<circle class="fill-accent" cx={x} cy={y} r={2} />
 </svg>
